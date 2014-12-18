b54311eb337a2b416bea7a4afb331357accda433
Andrew Buntine Of Doom <bunts@hhd.com.au>
2014-12-18 15:31:42 +1100

    Added fancy colours because I'm a legend

--- cd_git.py
+++ cd_git.py
@@ -3,12 +3,15 @@
 import re
 import subprocess as sub
 
+WARNING = '\033[93m'
+ENDC    = '\033[0m'
+
 try:
     result = sub.check_output("git branch", stderr=sub.STDOUT, shell=True)
     match  = re.search('\*\s\w+', result)
     branch = match.group(0)
 
     if not branch == "* master":
-        print "NOT ON MASTER"
+        print; print WARNING + "WARNING: NOT ON MASTER BRANCH" + ENDC; print
 except:
     pass
