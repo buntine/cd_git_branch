a08e60c281aa1cfd6fbf71207b311636e2f1fe56
Andrew Buntine Of Doom <bunts@hhd.com.au>
2014-12-18 15:21:51 +1100

    Got basic script working

--- cd_git.py
+++ cd_git.py
@@ -1,3 +1,14 @@
 #! /usr/bin/env python
 
+import re
+import subprocess as sub
 
+try:
+    result = sub.check_output("git branch", stderr=sub.STDOUT, shell=True)
+    match  = re.search('\*\s\w+', result)
+    branch = match.group(0)
+
+    if not branch == "* master":
+        print "NOT ON MASTER"
+except:
+    pass
